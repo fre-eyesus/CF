b432b8dbeeb445ced8f3d39a34851e83f1856d54
Fireweld Tesfaye <freyesus12@gmail.com>
2025-01-02 00:54:04 +0300

    changed the logic

--- WTL.py
+++ WTL.py
@@ -1,8 +1,7 @@
-word = input().lower()
-if len(word) >= 10:
-    first_char = word[0] 
-    last_char =  word[-1]
-    middle_length = len(word[1:-1])
-    print(first_char,middle_length,last_char)
-else:
-    print(word)
+n = int(input())
+for _ in range(n):
+     st = input()
+     if len(st) < 11:
+         print(st)
+     else: 
+        print(st[0] + str(len(st)-2) + st[-1])
