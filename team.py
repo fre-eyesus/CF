57684da1e6d38e82399f602ef6d4f6b593ce72f4
Fireweld Tesfaye <freyesus12@gmail.com>
2025-01-03 17:58:22 +0300

    solve the team problem in codeforce

---
+++ team.py
@@ -0,0 +1,17 @@
+n = int(input())
+flag = 0 
+
+for _ in range(n):
+    l = input().split(" ")
+    line = list(map(int, l))
+
+    
+    for j in range(len(line) - 2):  
+        if (line[j] == 1 and line[j + 1] == 1):  
+            flag += 1
+        elif (line[j] == 1 and line[j + 2] == 1):  
+            flag += 1
+        elif (line[j + 1] == 1 and line[j + 2] == 1):
+            flag += 1
+
+print(flag)
